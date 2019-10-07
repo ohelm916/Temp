1c90647c10a72cc053caf14ca5b3af259da7abee
Helminiak <ohelm916@uwsp.edu>
2019-10-07 17:20:12 -0500

    This is my initial value of the world value survey

---
+++ Temp code.R
@@ -0,0 +1,6 @@
+WVSData <-read.csv(file="C:\\Users\\ohelm916\\Documents\\WV_US.csv",
+                   header= TRUE, sep = ",")
+summary(WVSData$V23)
+str(WVSData)
+myReg=lm(V23~V59,WVSData)
+summary(myReg)
